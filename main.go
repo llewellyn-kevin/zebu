9895714845a899ce04a1062721f19821bb7aa5da
Kevin Llewellyn <llewellynkevin1@gmail.com>
2020-03-21 17:33:32 -0600

    Added documentation, reformatted main method, added error struct

--- main.go
+++ main.go
@@ -7,21 +7,7 @@ import(
 )
 
 func main() {
-	var(
-		namespace, action string 
-		args []string
-	)
-
-	command := os.Args[1]
-	args = os.Args[2:]
-
-	commandArr := strings.Split(command, ":")
-	if len(commandArr) > 1 {
-		namespace, action = commandArr[0], commandArr[1]
-	} else {
-		namespace, action = commandArr[1], commandArr[1]
-	}
-
+	namespace, action, args, _ := parseInput(os.Args[1:])
 	orderedArgs, flagArgs := parseArgs(args)
 
 	fmt.Println("NAMESPACE: " + namespace)
@@ -32,6 +18,36 @@ func main() {
 	}
 }
 
+type IllegalInput struct {
+	error string
+}
+
+func (i* IllegalInput) Error() string {
+	return i.error
+}
+
+func newIllegalInput() IllegalInput {
+	return IllegalInput{
+		error: "IllegalInput Detected",
+	}
+}
+
+func parseInput(input []string) (namespace string, action string, args []string, err error) {
+	commandArr := strings.Split(input[0], ":")
+	if len(commandArr) > 1 {
+		namespace, action = commandArr[0], commandArr[1]
+	} else {
+		namespace, action = commandArr[1], "default"
+	}
+	args = input[1:]
+	return
+}
+
+// parseArgs takes a slice of arguments and iterates through them to seperate them into 
+// a two seperate data structures. Any argument that begins with a '-' will be treated
+// as a flag, and add this argument to the flagArgs map with a key for whatever follows 
+// the hyphen and value being the next argument in the slice. Any other argument is 
+// appended to the orderedArgs slice.
 func parseArgs(args []string) (orderedArgs []string, flagArgs map[string]string) {
 	flagArgs = make(map[string]string)
 
